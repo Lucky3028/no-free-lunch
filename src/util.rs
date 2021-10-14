f50d5c44e893bcbd63ef50d538f6bd1339e835ef
Lucky <lucky3028@users.noreply.github.com>
2021-10-14 15:52:37 +0900

    [Change] embed周りをtraitでまとめる

--- src/util.rs
+++ src/util.rs
@@ -1,28 +1,24 @@
-pub mod discord_embeds {
-    use chrono::Utc;
-    use serenity::{builder::CreateEmbed, utils::Color};
+use chrono::Utc;
+use serenity::{builder::CreateEmbed, utils::Color};
 
-    pub fn default_embed() -> CreateEmbed {
-        CreateEmbed::default()
-            .timestamp(Utc::now().to_rfc3339())
-            .to_owned()
+pub trait MyEmbed {
+    fn time_footer(&mut self) -> CreateEmbed;
+    fn default_color(&mut self) -> CreateEmbed;
+    fn success_color(&mut self) -> CreateEmbed;
+    fn failure_color(&mut self) -> CreateEmbed;
+}
+
+impl MyEmbed for CreateEmbed {
+    fn time_footer(&mut self) -> CreateEmbed {
+        self.timestamp(Utc::now().to_rfc3339()).to_owned()
     }
-
-    pub fn colored_default_embed() -> CreateEmbed {
-        default_embed()
-            .color(Color::from_rgb(179, 159, 159))
-            .to_owned()
+    fn default_color(&mut self) -> CreateEmbed {
+        self.color(Color::from_rgb(179, 159, 159)).to_owned()
     }
-
-    pub fn default_success_embed() -> CreateEmbed {
-        default_embed()
-            .color(Color::from_rgb(50, 173, 240))
-            .to_owned()
+    fn success_color(&mut self) -> CreateEmbed {
+        self.color(Color::from_rgb(50, 173, 240)).to_owned()
     }
-
-    pub fn default_failure_embed() -> CreateEmbed {
-        default_embed()
-            .color(Color::from_rgb(245, 93, 93))
-            .to_owned()
+    fn failure_color(&mut self) -> CreateEmbed {
+        self.color(Color::from_rgb(245, 93, 93)).to_owned()
     }
 }
