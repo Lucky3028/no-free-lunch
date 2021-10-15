0931e3cc351ee8c7c8a3f5a86548c23ba5a34643
Lucky <lucky3028@users.noreply.github.com>
2021-10-15 23:19:42 +0900

    [Change] dockerを使わない形に

--- attachments/restart-docker.sh
+++ attachments/restart-docker.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-IMAGE=ghcr.io/lucky3028/no-free-lunch
-
-docker-compose down
-# https://stackoverflow.com/questions/49316462/how-to-update-existing-images-with-docker-compose
-docker rmi -f $(docker images | grep "$IMAGE" | awk '{print$3}')
-docker pull ${IMAGE}:latest
-# `--force-recreate`を指定しないと、前回まで使用していたイメージが存在しないというエラーが出て、続行するかどうかを選択しなければいけなくなる
-docker-compose up -d --force-recreate
+export $(grep -v '^#' .env | xargs -d '\n')
+chmod +x no-free-lunch
+DISCORD_TOKEN=$DISCORD_TOKEN APPLICATION_ID=$APPLICATION_ID ./no-free-lunch
